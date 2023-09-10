8a636919831a6c880c2c31f360cb4a85d5d871e3
Manomenu <88382777+Manomenu@users.noreply.github.com>
2023-09-10 23:36:34 +0200

    configure-buffers-fun-completed

--- glFun/WindowApp.h
+++ glFun/WindowApp.h
@@ -5,18 +5,18 @@
 #include "Camera.h"
 #include <GLFW/glfw3.h>
 
+#pragma region DataStructs
+
 struct WindowData
 {
 	GLFWwindow* window;
 	int width, height;
-
 	WindowData(std::string title, int winX, int winY)
 	{
 		this->window = glfwCreateWindow(winX, winY, title.c_str(), NULL, NULL);
 		this->width = winX;
 		this->height = winY;
 	}
-
 	~WindowData()
 	{
 		delete window;
@@ -27,14 +27,12 @@ struct CameraData
 {
 	Camera* camera;
 	float lastX, lastY;
-
 	CameraData(float winX, float winY)
 	{
 		this->camera = new Camera(0.0f, 0.0f, 9.0f, 0.0f, 1.0f, 0.0f, -90.0f, 0.0f);
 		this->lastX = winX / 2.0f;
 		this->lastY = winY / 2.0f;
 	}
-
 	~CameraData()
 	{
 		delete camera;
@@ -44,15 +42,15 @@ struct CameraData
 struct PointLight
 {
 	glm::vec3 position;
-
 	float constant;
 	float linear;
 	float quadratic;
-
 	glm::vec3 ambient;
 	glm::vec3 diffuse;
 	glm::vec3 specular;
 };
+
+#pragma endregion
 
 class WindowApp
 {
